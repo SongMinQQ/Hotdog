9b2fb9d5dc7dd0d48c1195fc033bc0a2b86da0c4
CMPIG <1027jwj@naver.com>
2023-12-12 22:39:45 +0900

    community

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -4,57 +4,57 @@ import { useNavigation } from '@react-navigation/native';
 import firebase from './firebase';
 
 const Community = () => {
-  const navigation = useNavigation();
-  const [selectedCategory, setSelectedCategory] = useState('가입인사');
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
+  // const navigation = useNavigation();
+  // const [selectedCategory, setSelectedCategory] = useState('가입인사');
+  // const [data, setData] = useState([]);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const itemsPerPage = 6;
 
-  useEffect(() => {
-    // Firebase에서 데이터 가져오는 로직
-    const fetchData = async () => {
-      const snapshot = await firebase.database().ref('posts').once('value');
-      const firebaseData = snapshot.val();
+  // useEffect(() => {
+  //   // Firebase에서 데이터 가져오는 로직
+  //   const fetchData = async () => {
+  //     const snapshot = await firebase.database().ref('posts').once('value');
+  //     const firebaseData = snapshot.val();
 
-      // Firebase Realtime Database의 구조 변경에 따라 코드 수정
-      const postArray = firebaseData ? Object.entries(firebaseData).map(([postId, postData]) => ({ postId, ...postData })) : [];
+  //     // Firebase Realtime Database의 구조 변경에 따라 코드 수정
+  //     const postArray = firebaseData ? Object.entries(firebaseData).map(([postId, postData]) => ({ postId, ...postData })) : [];
 
-      // 카테고리에 따라 데이터 필터링
-      const filteredData = postArray.filter(item => item.category === selectedCategory);
-      setData(filteredData);
-    };
+  //     // 카테고리에 따라 데이터 필터링
+  //     const filteredData = postArray.filter(item => item.category === selectedCategory);
+  //     setData(filteredData);
+  //   };
 
-    fetchData();
-  }, [selectedCategory]);
+  //   fetchData();
+  // }, [selectedCategory]);
 
-  const handleCategoryChange = category => {
-    setSelectedCategory(category);
-    setCurrentPage(1); // 카테고리 변경 시 페이지 초기화
-  };
+  // const handleCategoryChange = category => {
+  //   setSelectedCategory(category);
+  //   setCurrentPage(1); // 카테고리 변경 시 페이지 초기화
+  // };
 
-  const renderListItem = ({ item, index }) => (
-    <View>
-      <Text>{index + 1}. {item.title}</Text>
-    </View>
-  );
+  // const renderListItem = ({ item, index }) => (
+  //   <View>
+  //     <Text>{index + 1}. {item.title}</Text>
+  //   </View>
+  // );
 
-  const handleAddButtonPress = () => {
-    navigation.navigate('AddData');
-  };
+  // const handleAddButtonPress = () => {
+  //   navigation.navigate('AddData');
+  // };
 
-  const handleDeleteButtonPress = () => {
-    navigation.navigate('DeleteData');
-  };
+  // const handleDeleteButtonPress = () => {
+  //   navigation.navigate('DeleteData');
+  // };
 
-  const handleNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
+  // const handleNextPage = () => {
+  //   setCurrentPage(currentPage + 1);
+  // };
 
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
+  // const handlePrevPage = () => {
+  //   if (currentPage > 1) {
+  //     setCurrentPage(currentPage - 1);
+  //   }
+  // };
 
   return (
     <View>
